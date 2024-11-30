de0dfca2ebcad0a105d13e1c66323e9f54b54efb
Rasoolbi54 <smbrasoolbi138@gmail.com>
2024-11-30 19:04:59 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css';
 import Home from './components/Home/Home';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
-import Header from './components/header/Header';
+
 import Projects from './components/projects/Projects';
+import Header from './components/header/index';
 
 function App() {
   return (
    <div>
-    <Header />
+     <Header />
     <Home/>
     <About/>
     <Projects/>
